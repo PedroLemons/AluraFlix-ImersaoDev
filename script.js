aece23b4f7c21fd6ca64be556ffd07dcbf1825d3
PedroLemons <p.a.lemos1@gmail.com>
2023-01-30 13:38:52 -0300

    Iniciando ImersaoDev

---
+++ script.js
@@ -0,0 +1,35 @@
+// mobas
+var listaJogosMoba = ["https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/21779-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/32507-285x380.jpg"];
+
+document.write("<br> <h1> MOBA </h1>");
+
+for (var i = 0; i < listaJogosMoba.length; i++) {
+  document.write("<img src=" + listaJogosMoba[i] + ">");
+}
+
+// fps
+var listaJogosFps = ["https://static-cdn.jtvnw.net/ttv-boxart/32399_IGDB-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/488552-285x380.jpg"];
+
+document.write("<br> <h1> First Person Shooter </h1>");
+
+for (var i = 0; i < listaJogosFps.length; i++) {
+  document.write("<img src=" + listaJogosFps[i] + ">");
+}
+
+// rpg
+var listaJogosRpg = ["https://static-cdn.jtvnw.net/ttv-boxart/18122-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/490100-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/493597_IGDB-285x380.jpg"];
+
+document.write("<br> <h1> RPG </h1>");
+
+for (var i = 0; i < listaJogosRpg.length; i++) {
+  document.write("<img src=" + listaJogosRpg[i] + ">");
+}
+
+// battle royales
+var listaJogosBr = ["https://static-cdn.jtvnw.net/ttv-boxart/511224-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/33214-285x380.jpg", "https://static-cdn.jtvnw.net/ttv-boxart/493057-285x380.jpg"];
+
+document.write("<br> <h1> Battle Royale </h1>");
+
+for (var i = 0; i < listaJogosBr.length; i++) {
+  document.write("<img src=" + listaJogosBr[i] + ">");
+}
